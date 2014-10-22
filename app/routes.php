239f85c5f338d7d33da7f09577b6128996331cad
HAKEON <hakeon@gmail.com>
2014-10-21 20:11:34 -0400

    Lecture 5, part 2; Lecture 6 into Part 2 until Pre composer update

--- app/routes.php
+++ app/routes.php
@@ -1,34 +1,40 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-Route::get('/', function()
-{
-	return View::make('hello');
+# Homepage
+Route::get('/', function() {
+   return 'Welcome to Foobooks!';
 });
 
-Route::get('/books/{category}', function($category) {
-    return 'Here are all the book in the category of '.$category;
+Route::get('/list/{query?}', function($query) {
+    return 'List all the books';
 });
 
-Route::get('/new', function() {
-    $view =  '<form method = "POST">';
-    $view .= 'Title: <input type="text" name="title">';
-    $view .= '<input type="submit">';
-    $view .= '</form>';
-    return $view;
+// Display the form for a new book
+Route::get('/add', function() {
+
 });
 
-Route::post('/new', function() {
-    $input = Input::all();
-    print_r($input);
+// Process the form for a new book
+Route::post('/add', function() {
+
+});
+
+// Display the form to edit a book
+Route::get('/edit/{title}', function($title) {
+
+});
+
+// Process the form for the edit of a book
+Route::post('/add', function() {
+
+});
+
+// Practice route
+Route::get('/data', function() {
+    // Get the file
+    $books = File::get(app_path().'/database/books.json');
+    // Convert to an array
+    $books = json_decode($books,true);
+    // Return the file
+    return $books;
 });
